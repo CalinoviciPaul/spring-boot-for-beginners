79feba883ffad4e483b42b337893502df05196cf
Paul Calinovici <calinovicipaul@gmail.com>
2017-07-06 16:46:20 +0300

    added post request

--- src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -3,10 +3,11 @@ package com.in28minutes.springboot.controller;
 import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -30,6 +31,28 @@ public class SurveyController {
     public Question retrieveDetailsForQuestion(@PathVariable String surveyId,
                                                @PathVariable String questionId) {
         return surveyService.retrieveQuestion(surveyId, questionId);
+    }
+
+
+    // /surveys/{surveyId}/questions
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Void> addQuestionToSurvey(
+            @PathVariable String surveyId, @RequestBody Question newQuestion) {
+
+        Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+        if (question == null)
+            return ResponseEntity.noContent().build();
+
+        // Success - URI of the new resource in Response Header
+        // Status - created
+        // URI -> /surveys/{surveyId}/questions/{questionId}
+        // question.getQuestionId()
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+                "/{id}").buildAndExpand(question.getId()).toUri();
+
+        // Status
+        return ResponseEntity.created(location).build();
     }
 
 }
