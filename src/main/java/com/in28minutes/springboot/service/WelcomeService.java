0692d6015b6365413f38acf623573aabac57cb86
Paul Calinovici <calinovicipaul@gmail.com>
2017-07-06 17:50:48 +0300

    dynamic configuration properties

--- src/main/java/com/in28minutes/springboot/service/WelcomeService.java
+++ src/main/java/com/in28minutes/springboot/service/WelcomeService.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,9 +11,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class WelcomeService {
 
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     public String retrieveWelcomeMessage() {
         //Complex Method
-        return "Good Morning updated";
+        return this.welcomeMessage;
     }
 }
 
