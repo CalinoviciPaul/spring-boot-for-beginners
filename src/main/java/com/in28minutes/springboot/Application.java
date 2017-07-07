5f1ad84e99bd80e83bb802eabc0133413834d7db
Paul Calinovici <calinovicipaul@gmail.com>
2017-07-07 12:22:46 +0300

    introduction into profiles

--- src/main/java/com/in28minutes/springboot/Application.java
+++ src/main/java/com/in28minutes/springboot/Application.java
@@ -3,7 +3,9 @@ package com.in28minutes.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @ComponentScan("com.in28minutes")
 @SpringBootApplication
@@ -12,6 +14,14 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
+	}
+
+
+	//search for this bean in hal browser, it only appears in the dev profile
+	@Bean
+	@Profile("dev")
+	public String dummy(){
+		return "dummy bean";
 	}
 
 }
