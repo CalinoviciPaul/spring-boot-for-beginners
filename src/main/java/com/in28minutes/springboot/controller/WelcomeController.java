11ffd5d9949cd010dc86d3749081e35726810de4
Paul Calinovici <calinovicipaul@gmail.com>
2017-07-07 12:38:40 +0300

    Type safe configuration properties

--- src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
@@ -1,11 +1,15 @@
 package com.in28minutes.springboot.controller;
 
 
+import com.in28minutes.springboot.configuration.BasicConfiguration;
 import com.in28minutes.springboot.service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IrianLaptop on 7/6/2017.
@@ -17,9 +21,22 @@ public class WelcomeController {
     @Autowired
     private WelcomeService service;
 
+    @Autowired
+    private BasicConfiguration configuration;
+
     @RequestMapping("/welcome")
     public String welcome() {
         return service.retrieveWelcomeMessage();
+    }
+
+    @RequestMapping("/dynamic-configuration")
+    public Map dynamicConfiguration() {
+        // Not the best practice to use a map to store differnt types!
+        Map map = new HashMap();
+        map.put("messsage", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("key", configuration.isValue());
+        return map;
     }
 
 }
